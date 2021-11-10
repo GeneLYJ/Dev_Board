0bc6defca2dfde6faef7cc60e4f82b18f0c273bf
GeneLYJ <68850993+GeneLYJ@users.noreply.github.com>
2021-11-10 15:19:05 +0800

    Add files via upload

--- Asia_Dev.py
+++ Asia_Dev.py
@@ -8,6 +8,9 @@ Created on Tue Nov  9 17:25:15 2021
 import numpy as np
 import os 
 
+import argparse
+import cv2
+
 import sys
 
 txt_1 = np.array([1,2,3,4,5,6])
@@ -15,41 +18,84 @@ txt_1 = np.array([1,2,3,4,5,6])
 #np.savetxt('testing_file.txt', txt_1, delimiter=',')
 user_input = input('Your number?')
 
-if int(user_input) == 1:
-    save_path = '/home/mendel/mnt/sdcard'
-    file_name = 'testing_file.txt'
+def videoCapture():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
+  args = parser.parse_args()
+
+  args.camera_idx
+  cap = cv2.VideoCapture(args.camera_idx)
+
+  # Define the codec and create VideoWriter object
+  fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+  out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (780, 540))
+
+  while cap.isOpened():
+    ret, frame = cap.read()
     
-    completeName = os.path.join(save_path, file_name)
-    print(completeName)
+    if not ret:
+      break
     
-    f1 = open(completeName, "w+")
-    f1.write("file information")
-    f1.close()
-    
-elif int(user_input) == 2:
-    f1 = open('testing_here.txt', 'w+')
-    f1.write("file here")
-    f1.close()
-    print("done")
-    
-elif int(user_input) == 3:
-    save_path = '/home/mnt'
-    file_name = 'test_here.txt'
-    
-    completeName = os.path.join(save_path, file_name)
-    print(completeName)
-    
-    f1 = open(completeName, "w+")
-    f1.write("file information")
-    f1.close()
-    
-elif int(user_input) == 4:
-    save_path = '/home/mendel/AsiaM'
-    file_name = 'test_here.txt'
-    
-    completeName = os.path.join(save_path, file_name)
-    print(completeName)
-    
-    f1 = open(completeName, "w+")
-    f1.write("file information")
-    f1.close()
+    cv2_im = frame
+
+    cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+    cv2_im_rgb = cv2.resize(cv2_im_rgb, (780, 540))
+
+    # output the frame
+    out.write(cv2_im_rgb) 
+
+    cv2.imshow('frame', cv2_im)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+
+  cap.release()
+
+  # After we release our webcam, we also release the output
+  out.release() 
+
+  cv2.destroyAllWindows()
+
+
+
+def txt_saving():
+    if int(user_input) == 1:
+        save_path = '/home/mendel/mnt/sdcard'
+        file_name = 'testing_file.txt'
+        
+        completeName = os.path.join(save_path, file_name)
+        print(completeName)
+        
+        f1 = open(completeName, "w+")
+        f1.write("file information")
+        f1.close()
+        
+    elif int(user_input) == 2:
+        f1 = open('testing_here.txt', 'w+')
+        f1.write("file here")
+        f1.close()
+        print("done")
+        
+    elif int(user_input) == 3:
+        save_path = '/home/mnt'
+        file_name = 'test_here.txt'
+        
+        completeName = os.path.join(save_path, file_name)
+        print(completeName)
+        
+        f1 = open(completeName, "w+")
+        f1.write("file information")
+        f1.close()
+        
+    elif int(user_input) == 4:
+        save_path = '/home/mendel/AsiaM'
+        file_name = 'test_here.txt'
+        
+        completeName = os.path.join(save_path, file_name)
+        print(completeName)
+        
+        f1 = open(completeName, "w+")
+        f1.write("file information")
+        f1.close()
+
+if __name__ == '__main__':
+    videoCapture()
