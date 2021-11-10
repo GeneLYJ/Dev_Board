51446644044e34de94a09b9ce900814aa576eba2
GeneLYJ <68850993+GeneLYJ@users.noreply.github.com>
2021-11-10 11:35:41 +0800

    Add files via upload

--- Asia_Dev.py
+++ Asia_Dev.py
@@ -22,12 +22,12 @@ if int(user_input) == 1:
     completeName = os.path.join(save_path, file_name)
     print(completeName)
     
-    f1 = open(completeName, "w")
+    f1 = open(completeName, "w+")
     f1.write("file information")
     f1.close()
     
 elif int(user_input) == 2:
-    f1 = open('testing_here.txt', 'w')
+    f1 = open('testing_here.txt', 'w+')
     f1.write("file here")
     f1.close()
     print("done")
@@ -39,6 +39,39 @@ elif int(user_input) == 3:
     completeName = os.path.join(save_path, file_name)
     print(completeName)
     
-    f1 = open(completeName, "w")
+    f1 = open(completeName, "w+")
+    f1.write("file information")
+    f1.close()
+    
+elif int(user_input) == 4:
+    save_path = 'home/mendel/AsiaM'
+    file_name = 'test_here.txt'
+    
+    completeName = os.path.join(save_path, file_name)
+    print(completeName)
+    
+    f1 = open(completeName, "w+")
+    f1.write("file information")
+    f1.close()
+    
+elif int(user_input) == 5:
+    save_path = 'home/AsiaM'
+    file_name = 'test_here.txt'
+    
+    completeName = os.path.join(save_path, file_name)
+    print(completeName)
+    
+    f1 = open(completeName, "w+")
+    f1.write("file information")
+    f1.close()
+    
+elif int(user_input) == 6:
+    save_path = 'home/mnt/sdcard'
+    file_name = 'test_here.txt'
+    
+    completeName = os.path.join(save_path, file_name)
+    print(completeName)
+    
+    f1 = open(completeName, "w+")
     f1.write("file information")
     f1.close()
